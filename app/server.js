e092f5670a6520175cadda57a9c5410373a6b8b4
abreloishere <babyshome022@gmail.com>
2025-12-26 15:07:40 +0300

    adding backend cors to gi

--- app/server.js
+++ app/server.js
@@ -11,7 +11,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // your frontend local dev
+    methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 
